7167e581732ed8ba892a65c087379cac1077a00e
greentea <tysh3@yandex.ru>
2020-07-12 18:33:41 +0300

    set_my_commands, now supports ip : port from json

--- main.py
+++ main.py
@@ -15,12 +15,15 @@ s.headers.update({
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'
 })
 
-welcome_text = "Что бы получить magnet-ссылку, наберите /magnet *ссылка на тему рутрекера*\nНапример: /magnet https://rutracker.org/forum/viewtopic.php?t=1149720"
+welcome_text = "Что бы получить список комманд, наберите /"
+command_list = ("magnet - сделать из ссылки на тему rutracker.org magnet-ссылку",
+                "register - зарегистрировать сервер с transmission-remote. /register <address> <port> <login> <password>",
+                "add - добавить на сервер с вашими данными торрент по ссылке на тему rutacker.org")
 
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=welcome_text)
-
+    context.bot.set_my_commands(commands=command_list)
 
 def magnet(update, context):
     url = context.args[0]
@@ -37,18 +40,20 @@ def register(update, context):
         data[user_id] = users
     with open('data.json', 'w') as file:
         json.dump(data, file, indent=4)
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Вы добавили сервер {0}:{1}, ваш id - {2}".format(credentials[0], credentials[1], user_id))
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Вы добавили сервер {0}:{1}".format(credentials[0], credentials[1]))
 
 
 def add(update, context):
     url = context.args[0]
-    user_id = update.message.from_user['id']
+    user_id_quotes = '{}'.format(update.message.from_user['id'])
     with open('data.json', 'r') as file:
         data = json.load(file)
-    transremote_username = data["{}".format(user_id)]["username"]
-    transremote_password = data["{}".format(user_id)]["password"]
+    transremote_address = data[user_id_quotes]["address"]
+    transremote_port = data[user_id_quotes]["port"]
+    transremote_username = data[user_id_quotes]["username"]
+    transremote_password = data[user_id_quotes]["password"]
     magnet_link = get_link(url, s)
-    subprocess.call(['transmission-remote', '-n', transremote_username + ':' + transremote_password, '-a', magnet_link])
+    subprocess.call(['transmission-remote', transremote_address + ':' + transremote_port, '-n', transremote_username + ':' + transremote_password, '-a', magnet_link])
 
 
 start_handler = CommandHandler('start', start)
