7ca37487653a4cd01acdad675a80c382b679a469
CondensedTea <tysh3@yandex.ru>
2020-08-26 14:26:35 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from transmission_rpc import Client
-
-c = Client(address="shchepkina60.duckdns.org", port=9091, username="kotuxa", password="Korokino2020")
-
-item = 1
-
-for torrent in c.get_torrents():
-    print(torrent.name)
